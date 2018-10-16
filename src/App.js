498c01e8a8e995b5b706d9e96162ff3344123f8c
Ryan Groesch <ryan.groesch@gmail.com>
2018-10-15 17:56:52 -0700

    new state introduced

--- src/App.js
+++ src/App.js
@@ -3,39 +3,42 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-
-    const list = [ 
-      {
-        title: 'REACT',
-        url: 'https://facebook.github.io/react/',
-        author: 'Jimmay',
-        num_comments: 3,
-        points: 7,
-        objectId: 0,
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [ 
+        {
+          title: 'REACT',
+          url: 'https://facebook.github.io/react/',
+          author: 'Jimmay',
+          num_comments: 3,
+          points: 7,
+          objectId: 0,
+        },
+        {
+          title: 'REDUX',
+          url: 'https://github.com/reactjs/redux',
+          author: 'Danjamin',
+          num_comments: 1,
+          points: 5,
+          objectId: 1,
+        }
+      ],
+      message: {
+        greeting: 'Hallo, Wilkommen zu meine React App!',
+        userName: 'MonsieurFluffNStuff',
+        psw: true,
       },
-      {
-        title: 'REDUX',
-        url: 'https://github.com/reactjs/redux',
-        author: 'Danjamin',
-        num_comments: 1,
-        points: 5,
-        objectId: 1,
-      }
-    ]
-
-    let message = {
-    message: 'Hallo, Wilkommen zu meine React App!',
-    userName: 'MonsieurFluffNStuff',
-    psw: true,
     }
+  }
+  render() {
 
     return (
       <div className="App">
-        <h1>{message.message}</h1>
-        <p>Wilkommen {message.userName}, haben Sie eine Guten Tag</p>
+        <h1>{this.state.message.greeting}</h1>
+        <p>Wilkommen {this.state.message.userName}, haben Sie eine Guten Tag</p>
         <ul>
-          {list.map(element => {
+          {this.state.list.map(element => {
             return <li key={element.objectId}>
             <span>Title: <a href={element.url}>{element.title}</a></span>
             <span> Author: {element.author}</span>
